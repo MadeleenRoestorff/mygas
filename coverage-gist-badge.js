3fa94fde991d9c9e7e2a7a5a47b72e7f6d7403a4
MadeleenRoestorff <76064899+MadeleenRoestorff@users.noreply.github.com>
2023-01-25 13:50:15 +0000

    Create coverage-gist-badge.js

---
+++ coverage-gist-badge.js
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// import core from "@actions/core";
+const core = require("@actions/core");
+// const axios = require("axios");
+// import axios from "axios";
+
+// const config = {
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `token ${core.getInput("auth")}`
+//   }
+// };
+
+const content = {
+  schemaVersion: 1,
+  label: core.getInput("label"),
+  message: core.getInput("message"),
+  color: "green"
+};
+
+const filename = core.getInput("filename");
+
+const request = JSON.stringify({ files: { [filename]: { content: JSON.stringify(content) } } });
+
+// await axios.post(
+//   `https://gist.github.com/MadeleenRoestorff/${core.getInput("gistID")}/`,
+//   request,
+//   config
+// );
+
+console.log(request);
