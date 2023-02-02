a80db34f03855c0e491ee8021c1664d8358239d3
Maria Roestorff <maria.roestorff@gmail.com>
2023-02-02 10:15:59 +0000

    GAS-20: updated workflow file

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,15 +3,16 @@
 if npx eslint --max-warnings 0 "src/"
 then
     echo "ESlint exit code: $?"
-    echo "ES Lint Successful"
+    echo "ESLint Successful"
 else
-    echo "eslint exit code: $?"
+    echo "ESLint exit code: $?"
     echo "Failed ESlint" >&2
     exit 1
 fi
 
 if npx prettier --check "src/**/*.ts"
 then
+    echo "Prettier exit code: $?"
     echo "Prettier Completed"
 else
     echo "Prettier Error" >&2
